ee131d68405787a771bcb103af4097b2f8504621
hkd145 <125806064+hkd145@users.noreply.github.com>
2023-04-18 01:32:57 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ UUID=${UUID:-'d7b78201-ce99-4a64-9ccd-a0e87af33453'}
 
 # 其他Paas保活
 PAAS1_URL= https://test0418.onrender.com
-PAAS2_URL=
+PAAS2_URL= https://app-hkd145.cloud.okteto.net
 PAAS3_URL=
 PAAS4_URL=
 PAAS5_URL=
